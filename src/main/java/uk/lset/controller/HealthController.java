886022ce1a4ad46e0db272eaa5eb0a347f446223
Andrei-Razvan <camenicandreirazvan@yahoo.com>
2025-02-17 10:58:51 +0000

    first app

---
+++ src/main/java/uk/lset/controller/HealthController.java
@@ -0,0 +1,15 @@
+package uk.lset.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	
+	@GetMapping("/healthCheck")
+	public String healthCheck() {
+		return "Connected";
+	}
+	
+
+}
